eb5a962317da623e4f97015f07eb784f44210190
RazKirsha <81920913+RazKirsha@users.noreply.github.com>
2021-04-18 14:04:49 +0300

    Calculator fixed

--- Week2/Day5/DailyChallenge/DC.js
+++ Week2/Day5/DailyChallenge/DC.js
@@ -1,7 +1,7 @@
 let bottles = +prompt("How many bottles of beer in the wall?:");
 let les = 1;
 let fname;
-let verb;
+let verb = 'it';
 
 while (bottles>=0){
     while (bottles>les){
@@ -14,10 +14,6 @@ while (bottles>=0){
         console.log(`Take ${les} down, pass ${verb} around`);
         bottles -= les;
         les += 1;
-        if (bottles == 1){
-            verb = 'it';
-            fname = 'bottle'
-        }
         console.log(`${bottles} ${fname} of beer on the wall`);
     } 
     les = bottles;
@@ -27,7 +23,3 @@ while (bottles>=0){
     console.log(`0 bottles of beer on the wall`);
     break
 }
-
-// let word = "bottle";
-// let word2 = word + 's';
-// console.log(word2);
--- Week2/Day5/Exercise/Ex2/Ex2.js
+++ Week2/Day5/Exercise/Ex2/Ex2.js
@@ -1,15 +1,156 @@
-let arr = [];
-let arr1 = [0];
+// SHITTY CODE!!!!
+// let res;
+// let arr = [];
+// let arr1 = [0];
+// let arr2 = [];
+// let ttc = 0;
+// let turn = 0;
+// // function number(num){
+//     //     if(num !== num !== "+" && num !== "-" && num !== "/" && num !== "*"){
+//         //         arr.push(num);
+//         //         let n = parseInt(arr.join(''));
+//         //         console.log(n);
+//         //         arr1.push(n);
+//         //         arr1.shift();
+//         //     }
+//         //     console.log("arr1 = " + arr1);
+//         //     return arr1;
+//         // }
+        
+//         // console.log("arr1 = " + arr1);
+//         // arr2.push(arr1);
+//         // arr1.shift();
+//         // console.log("arr2 = " + arr2);
+        
+// function number(num){
+//     arr.push(num);
+//     let n = parseInt(arr.join(''));
+//     arr1.push(n);
+//     arr1.shift();
+//     console.log("arr1 = " + arr1);
+//     ttc += 1;
+//     return arr1;
+// }
+
+// function operator (operator) {
+//     arr = [];
+//     turn += 1;
+//     arr2.unshift(arr1[0]);
+//     console.log("arr2 = " + arr2);
+//     arr1 = [0];
+//     console.log("res = " + res);
+//     console.log("turn = " + turn);
+//     if (arr2.length > 1){
+//         if (turn % 2 == 0 && turn > 1){
+//             if (operator === "+"){
+//                 res = arr2[0] + arr2[1];
+//                 arr2 = [res];
+//                 console.log("arr2 = " + arr2);
+//                 console.log("res = " + res);
+//                 // console.log("ttc = " + ttc);
+//                 return res;
+//             }
+//             if (operator === "-"){
+//                 if (ttc == arr2[0].toString().length){
+//                     res = arr2[0];
+//                     // console.log("this is if");
+//                     // console.log("TTC = "+ttc);
+//                     // console.log("This is arr2[0].length = " + arr2[0].toString().length);
+//                     // console.log("This is arr2[0] = " + arr2[0] );
+//                     // console.log("This is arr2[1] = " + arr2[1] );
+//                     // console.log("This is arr1[0] = " + arr1[0] );
+//                 } else {
+//                     res = arr2[1] - arr2[0];
+//                     // console.log("this is else");
+//                     // console.log("TTC = "+ttc);
+//                     // console.log("This is arr2[0].length = " + arr2[0].toString().length);
+//                     // console.log("This is arr2[0] = " + arr2[0] );
+//                     // console.log("This is arr2[1] = " + arr2[1] );
+//                     // console.log("This is arr1[0] = " + arr1[0] );
+//                 }
+//                 arr2 = [res];
+//                 console.log("arr2 = " + arr2);
+//                 console.log("res = " + res);
+//                 // console.log("ttc = " + ttc);
+//                 return res;
+//             }
+//             if (operator === "*"){
+//                 if (ttc == arr2[0].toString().length){
+//                     res = arr2[0];
+//                     // console.log("this is if");
+//                     // console.log("TTC = "+ttc);
+//                     // console.log("This is arr2[0].length = " + arr2[0].toString().length);
+//                     // console.log("This is arr2[0] = " + arr2[0] );
+//                     // console.log("This is arr2[1] = " + arr2[1] );
+//                     // console.log("This is arr1[0] = " + arr1[0] );
+//                 } else {
+//                     res = arr2[1] * arr2[0];
+//                     // console.log("this is else");
+//                     // console.log("TTC = "+ttc);
+//                     // console.log("This is arr2[0].length = " + arr2[0].toString().length);
+//                     // console.log("This is arr2[0] = " + arr2[0] );
+//                     // console.log("This is arr2[1] = " + arr2[1] );
+//                     // console.log("This is arr1[0] = " + arr1[0] );
+//                 }
+//                 arr2 = [res];
+//                 console.log("arr2 = " + arr2);
+//                 console.log("res = " + res);
+//                 // console.log("ttc = " + ttc);
+//                 return res;
+//             }
+//             if (operator === "/"){
+//                 if (ttc == arr2[0].toString().length){
+//                     res = arr2[0];
+//                     // console.log("this is if");
+//                     // console.log("TTC = "+ttc);
+//                     // console.log("This is arr2[0].length = " + arr2[0].toString().length);
+//                     // console.log("This is arr2[0] = " + arr2[0] );
+//                     // console.log("This is arr2[1] = " + arr2[1] );
+//                     // console.log("This is arr1[0] = " + arr1[0] );
+//                 } else {
+//                     res = arr2[1] / arr2[0];
+//                     // console.log("this is else");
+//                     // console.log("TTC = "+ttc);
+//                     // console.log("This is arr2[0].length = " + arr2[0].toString().length);
+//                     // console.log("This is arr2[0] = " + arr2[0] );
+//                     // console.log("This is arr2[1] = " + arr2[1] );
+//                     // console.log("This is arr1[0] = " + arr1[0] );
+//                 }
+//                 arr2 = [res];
+//                 console.log("arr2 = " + arr2);
+//                 console.log("res = " + res);
+//                 // console.log("ttc = " + ttc);
+//                 return res;
+//             }
+//         }
+//     }
+//     // arr2 = [res];
+//     // console.log("res = " + res);
+//     // return res;
+//     // console.log("ttc = " + ttc);
+//     // console.log("arr1 = " + arr1);
+//     // console.log("arr2 = " + arr2);
+// }
+
+
+let str = '';
 
 function number(num){
-    if(num !== num !== "+" && num !== "-" && num !== "/" && num !== "*"){
-        arr.push(num);
-        let n = parseInt(arr.join(''));
-        console.log(n);
-        arr1.push(n);
-        arr1.shift();
-        console.log(arr1);
-        return arr1;
-    }
+    str = str + num.toString();
+    document.getElementById("calc").innerHTML = str;
 }
 
+function equal(){
+    let ev = eval(str);
+    str = ev.toString();
+    document.getElementById("calc").innerHTML = ev;
+}
+
+function reset(){
+    str = '';
+    document.getElementById("calc").innerHTML = 0;
+}
+
+function clr(){
+    document.getElementById("calc").innerHTML = 0;
+}
